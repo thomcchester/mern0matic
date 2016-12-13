f2173fd0005ba29e63fd1dee4a06f887e4328a29
thomcchester <thomcchester@gmail.com>
2016-12-13 17:03:38 -0600

    remove direcotries

--- poo/client/src/Client.js
+++
@@ -1,8 +0,0 @@
-
-const axios = require('axios')
-
-var num = axios.get('/defaults').then(result => {
-              console.log(result.data)
-              return result.data;
-          })
-console.log(num, "num")
--- poo/models/default.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-var Default_Values = new Schema({
-	testVal: {type:Number},
-});
-
-module.exports = mongoose.model("Default_Values", Default_Values);
