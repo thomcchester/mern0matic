4c1c312f18b19867b4cbec52997cf213d6e865d7
thomcchester <thomcchester@gmail.com>
2016-12-13 15:23:08 -0600

    i cant think any more

---
+++ poo/client/src/Client.js
@@ -0,0 +1,8 @@
+
+const axios = require('axios')
+
+var num = axios.get('/defaults').then(result => {
+              console.log(result.data)
+              return result.data;
+          })
+console.log(num, "num")
